718dfbf2e2253ea462503230e022fe09c6357c16
Rathan <60232524+kokilarathan-siva01@users.noreply.github.com>
2021-03-16 13:24:21 +0000

    GUI Updated v2.0
    nothing has been updated, yup,nope,yipyip

--- Stickbot_GUI.py
+++ Stickbot_GUI.py
@@ -4,17 +4,37 @@ from tkinter import *
 Stickbot = Tk()  # creates a tkinter object (GUI)
 Stickbot.title("Stick-Bot Interactive Interface") # the title of the program/GUI
 
-#Stickbot.geometry("600x400")# this can be used to change the screen resoultion of the GUI.
-#Stickbot.config(bg='red')
+
+
+Stickbot.geometry("600x400")
 
 
 # The main tital of the display screen
-Stickbot_title = Label(Stickbot, text="Stick Bot", width=50, borderwidth=5)
+Stickbot_title = Label(Stickbot, text="Stick Bot", width=60, borderwidth=5)
 Stickbot_title.grid(row=0, column = 0, columnspan=8, padx= 10, pady=10)
 Stickbot_title.config(bg='lightgreen')
 
 def buttons_random():
-    return 0
+    return "What is your name"
+
+def UpdateError():
+    rows = []
+
+    for i in range(5):
+
+        cols = []
+
+        for j in range(4):
+
+            e = Entry(relief=GROOVE)
+
+            e.grid(row=i, column=j, sticky=NSEW)
+
+            e.insert(END, '%d.%d' % (i, j))
+
+            cols.append(e)
+
+        rows.append(cols)
     
 
 # Creating variable's buttons.
@@ -28,24 +48,27 @@ button_Exit = Button(Stickbot, text="EXIT", padx=10, pady=10, command=buttons_ra
 
 # font detail for text.
 Font_tuple = ("Comic Sans MS", 20, "bold")
+frame = Frame(Stickbot, background='yellow', borderwidth=20, relief=RAISED)
  
 # update error log space
 updateErrorLog = Message(Stickbot, text="Updates and Error log")
 updateErrorLog.grid(row=1, column=3, columnspan=2, rowspan=5)
-updateErrorLog.config(bg='lightgreen')
+updateErrorLog.config(bg='lightgreen', borderwidth=2, relief="raised")
 updateErrorLog.configure(font=Font_tuple)
 
 # graph location
-updateErrorLog = Message(Stickbot, text="Graph goes here")
-updateErrorLog.grid(row=1, column=5, columnspan=4, rowspan=3)
-updateErrorLog.config(bg='lightgreen')
-updateErrorLog.configure(font=Font_tuple)
+graphLocation = Message(Stickbot, text="Graph Health ")
+graphLocation.grid(row=1, column=5, columnspan=4, rowspan=3)
+graphLocation.config(bg='lightgreen')
+graphLocation.configure(font=Font_tuple)
 
 # Crop health location
-updateErrorLog = Message(Stickbot, text="CROP HEALTH")
-updateErrorLog.grid(row=4, column=5, columnspan=4, rowspan=2)
-updateErrorLog.config(bg='lightgreen')
-updateErrorLog.configure(font=Font_tuple)
+cropHealth = Message(Stickbot, text="CROP HEALTH")
+cropHealth.grid(row=4, column=5, columnspan=4, rowspan=2)
+cropHealth.config(bg='cyan')
+cropHealth.configure(font=Font_tuple)
+
+
 
 
 
