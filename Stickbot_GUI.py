4f9369695bc9aa01ab9da87157fb9a942d719c65
Rathan <60232524+kokilarathan-siva01@users.noreply.github.com>
2021-04-26 14:18:35 +0100

    STICKBOT-VERSION2-UPDATEUNKNOWN
    IDK WHAT I ADDED

--- Stickbot_GUI.py
+++ Stickbot_GUI.py
@@ -9,11 +9,184 @@ import tkinter.scrolledtext as st
 from time import strftime
 import tkinter
 
-LARGEFONT = ("Comic Sans MS", 30, "bold")
+import matplotlib.pyplot as plt
+
+
+#LARGEFONT = ("Comic Sans MS", 30, "bold")
 mediumfont = ("Comic Sans MS", 20, "bold")
 smallfont = ("Comic Sans MS", 15, "bold")
 
 cropHeath = 40
+c02_score = None
+temperature_score = None
+ph_score = None
+oxygen_score = None
+
+
+def c02(): # This checks all the imported values against the correct mesuerments to make sure they are in the bounds stated and creates 1 part of the score for the overall crop health graph. 
+ 
+  global c02_score
+  c02_score = 0 # Overall c02 score. 
+  c02_input = 1200, 1100, 1000, 1000, 2000, 5, 76, 3, 54, 3, 45, 2, 4, 2, 43, 2, 70, 8,19, 20 # To be replaced with the imported variable name. 
+
+  for x in c02_input:
+   if x <= 1300 and x >= 1000:
+     print ("Green")
+     c02_score += 1
+
+   elif x > 1310 and x < 1001:
+      print ("Red")
+
+   else:
+     print ("Red")
+ 
+
+  if c02_score == 24:
+    c02_score += 1
+
+
+
+def c02chart(): # Creates the graph to show all of the readings taken over the last stated time period.
+
+
+ c02x = 2, 3, 4, 5, 6, 7, 8, 9, 1 # To be replaced with the imported variable name. 
+ c02y = 5, 3, 2, 5, 6, 7, 8, 9, 2 # To be replaced with the imported variable name. 
+ xpoints = c02x
+ ypoints = c02y
+
+ plt.plot(xpoints, ypoints, 'o')
+
+ plt.xlabel("Time")
+ plt.ylabel("Measurement (PPM)")
+ plt.title("C02 graph")
+ plt.show()
+
+def temperature(): # This checks all the imported values against the correct mesuerments to make sure they are in the bounds stated and creates 1 part of the score for the overall crop health grawater. 
+ 
+  global temperature_score
+  temperature_score = 0 # Overall water score. 
+  temperature_input = 1200, 1100, 1000, 1000, 2000, 5, 76, 3, 54, 3, 45, 2, 4, 2, 43, 2, 70, 8,19, 20 # To be replaced with the imported variable name. 
+
+  for x in temperature_input:
+   if x <= 78 and x >= 70:
+     print ("Green")
+     temperature_score += 1
+
+   elif x > 78 and x < 70:
+      print ("Red")
+
+   else:
+     print ("Red")
+ 
+
+  if temperature_score == 24:
+    temperature_score += 1
+  
+
+
+def temperaturechart(): # Creates the grawater to show all of the readings taken over the last stated time period.
+
+ import matplotlib.pyplot as plt
+
+ temperaturex = 2, 3, 4, 5, 6, 7, 8, 9, 1 # To be replaced with the imported variable name. 
+ temperaturey = 5, 3, 2, 5, 6, 7, 8, 9, 2 # To be replaced with the imported variable name. 
+ xpoints = temperaturex
+ ypoints = temperaturey
+
+ plt.plot(xpoints, ypoints, 'o')
+
+ plt.xlabel("Time")
+ plt.ylabel("Measurement (PPH)")
+ plt.title("Temperature graph")
+ plt.show()
+
+temperature() # Calls the water function.
+print("temperature score = ", temperature_score, "/ 25") # Displays the water score out of 25.
+
+def ph(): # This checks all the imported values against the correct mesuerments to make sure they are in the bounds stated and creates 1 part of the score for the overall crop health graph. 
+ 
+  global ph_score
+  ph_score = 0 # Overall ph score. 
+  ph_input = 1200, 1100, 1000, 1000, 2000, 5, 76, 3, 54, 3, 45, 2, 4, 2, 43, 2, 70, 8,19, 20 # To be replaced with the imported variable name. 
+
+  for x in ph_input:
+   if x <= 8.3 and x >= 5.5:
+     print ("Green")
+     ph_score += 1
+
+   elif x > 8.4 and x < 5.5:
+      print ("Red")
+
+   else:
+     print ("Red")
+ 
+
+  if ph_score == 24:
+    ph_score += 1
+  
+
+
+def phchart(): # Creates the graph to show all of the readings taken over the last stated time period.
+
+ import matplotlib.pyplot as plt
+
+ phx = 2, 3, 4, 5, 6, 7, 8, 9, 1 # To be replaced with the imported variable name. 
+ phy = 5, 3, 2, 5, 6, 7, 8, 9, 2 # To be replaced with the imported variable name. 
+ xpoints = phx
+ ypoints = phy
+
+ plt.plot(xpoints, ypoints, 'o')
+
+ plt.xlabel("Time")
+ plt.ylabel("Measurement (PH)")
+ plt.title("PH graph")
+ plt.show()
+
+ph() # Calls the ph function.
+print("PH score = ", ph_score, "/ 25") # Displays the ph score out of 25.
+
+def oxygen(): # This checks all the imported values against the correct mesuerments to make sure they are in the bounds stated and creates 1 part of the score for the overall crop health graph oxygen. 
+ 
+  global oxygen_score
+  oxygen_score = 0 # Overall oxgen score. 
+  oxygen_input = 1200, 1100, 1000, 1000, 2000, 5, 76, 3, 54, 3, 45, 2, 4, 2, 43, 2, 70, 8,19, 20 # To be replaced with the imported variable name. 
+
+  for x in oxygen_input:
+   if x <= 6 and x >= 5:
+     print ("Green")
+     oxygen_score += 1
+
+   elif x > 6.1 and x < 4.9:
+      print ("Red")
+
+   else:
+     print ("Red")
+ 
+
+  if oxygen_score == 24:
+    oxygen_score += 1
+  
+
+
+def oxygenchart(): # Creates the graph oxygen to show all of the readings taken over the last stated time period.
+
+ import matplotlib.pyplot as plt
+
+ oxygenx = 2, 3, 4, 5, 6, 7, 8, 9, 1 # To be replaced with the imported variable name. 
+ oxygeny = 5, 3, 2, 5, 6, 7, 8, 9, 2 # To be replaced with the imported variable name. 
+ xpoints = oxygenx
+ ypoints = oxygeny
+
+ plt.plot(xpoints, ypoints, 'o')
+
+ plt.xlabel("Time")
+ plt.ylabel("Measurement (MG/L)")
+ plt.title("OXYGEN graph")
+ plt.show()
+
+oxygen() # Calls the oxgen function.
+print("OXYGEN score = ", oxygen_score, "/ 25") # Displays the oxygen score out of 25.
+
 
 class Stickbot(tk.Tk):
 
@@ -105,11 +278,11 @@ class Mainpage(tk.Frame):
 
         progress_Bar.grid(row=4, column=5, rowspan=2)
 
-        healthdigit = "Crop's Health : " + str(cropHeath) + "/100"
+        healthdigit = str(cropHeath) + "/100"
 
-        healthintext = Label(self, text=healthdigit, background="cyan", foreground="black", borderwidth=7,
+        healthintext = Label(self, text=healthdigit, foreground="black", borderwidth=2,
                              font=smallfont, relief='flat')
-        healthintext.grid(row=3, column=5)
+        healthintext.grid(row=5, column=5)
 
         # Clock widget
 
@@ -184,14 +357,16 @@ class CO2(tk.Frame):
         # Making the Log read only
         updateErrorLog.configure(state='disabled')
 
-        # graph location
-        graphLocation = Message(self, text="Graph Health ")
+       # graph location
+        graphLocation = Button(self, text="Graph Health ", command=lambda:[c02(), c02chart()])
         graphLocation.grid(row=1, column=5, columnspan=4, rowspan=3)
-        graphLocation.config(background='lightgreen')
-        graphLocation.configure(font=mediumfont)
+
 
     def close(self):
         self.master.quit()
+    
+
+    
 
 
 # window frame for Temperature stats 
@@ -200,7 +375,7 @@ class Temperature(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         # label of frame/ Title of the page. plus the location using grid
-        Stickbot_title = Label(self, text="       Carbon Dioxide - Stats", width=30, borderwidth=5, font=mediumfont,
+        Stickbot_title = Label(self, text="       Temperature - Stats", width=30, borderwidth=5, font=mediumfont,
                                background='lightgreen', foreground='black')
 
         Stickbot_title.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
@@ -247,11 +422,9 @@ class Temperature(tk.Frame):
         # Making the Log read only
         updateErrorLog.configure(state='disabled')
 
-        # graph location
-        graphLocation = Message(self, text="Graph Health ")
+       # graph location
+        graphLocation = Button(self, text="Graph Health ", command=lambda:[temperature(), temperaturechart()])
         graphLocation.grid(row=1, column=5, columnspan=4, rowspan=3)
-        graphLocation.config(background='lightgreen')
-        graphLocation.configure(font=mediumfont)
 
     def close(self):
         self.master.quit()
@@ -264,7 +437,7 @@ class PH(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         # label of frame/ Title of the page. plus the location using grid
-        Stickbot_title = Label(self, text="       Carbon Dioxide - Stats", width=30, borderwidth=5, font=mediumfont,
+        Stickbot_title = Label(self, text="       PH - Stats", width=30, borderwidth=5, font=mediumfont,
                                background='lightgreen', foreground='black')
 
         Stickbot_title.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
@@ -312,10 +485,8 @@ class PH(tk.Frame):
         updateErrorLog.configure(state='disabled')
 
         # graph location
-        graphLocation = Message(self, text="Graph Health ")
+        graphLocation = Button(self, text="Graph Health ", command=lambda:[ph(), phchart()])
         graphLocation.grid(row=1, column=5, columnspan=4, rowspan=3)
-        graphLocation.config(background='lightgreen')
-        graphLocation.configure(font=mediumfont)
 
     def close(self):
         self.master.quit()
@@ -328,14 +499,14 @@ class Oxygen(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         # label of frame/ Title of the page. plus the location using grid
-        Stickbot_title = Label(self, text="       Carbon Dioxide - Stats", width=30, borderwidth=5, font=mediumfont,
+        Stickbot_title = Label(self, text="       Oxygen - Stats", width=30, borderwidth=5, font=mediumfont,
                                background='lightgreen', foreground='black')
 
         Stickbot_title.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
         #  creating buttons and assigning them to their respective frames/page.
         button_mainmenu = ttk.Button(self, text="CO2 Level",
-                                     command=lambda: controller.change_frame(Mainpage))
+                                     command=lambda: controller.change_frame(CO2))
         button_mainmenu.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
 
         button_Temperature = ttk.Button(self, text="Temperature Level",
@@ -375,11 +546,11 @@ class Oxygen(tk.Frame):
         # Making the Log read only
         updateErrorLog.configure(state='disabled')
 
+
         # graph location
-        graphLocation = Message(self, text="Graph Health ")
+        graphLocation = Button(self, text="Graph Health ", command=lambda:[oxygen(), oxygenchart()])
         graphLocation.grid(row=1, column=5, columnspan=4, rowspan=3)
-        graphLocation.config(background='lightgreen')
-        graphLocation.configure(font=mediumfont)
+
 
     def close(self):
         self.master.quit()
